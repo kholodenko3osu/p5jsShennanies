04c96803ccecce0943465448a134608e8f814354
David A Kholodenko <kholodenko.3@osu.edu>
2023-11-16 22:31:06 -0500

    test

---
+++ iteration1script.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(1920, 1080);
+  background(0);
+}
+
+function draw() {
+  // Your drawing code here (if needed)
+}
+
+function mouseClicked() {
+  fill(0, 0, 255); // Set fill color to blue
+  ellipse(mouseX, mouseY, 50, 50); // Draw a circle at the mouse position
+  }
---
+++ iteration2script.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(1920, 1080);
+  background(0);
+}
+
+function draw() {
+}
+
+function mouseClicked() {
+  fill(0, 0, 255); 
+  ellipse(mouseX, mouseY, 50, 50); 
+  let puddleNum = puddleNum + 1
+}
+
+function keyPressed() {
+  if (keyCode === BACKSPACE) {
+    background(0);
+  }
+}
